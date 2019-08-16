da7500d2bc480f21ede550232cf6907f46a4c845
mts513 <34582160+mts513@users.noreply.github.com>
2019-08-15 17:59:34 -0700

    completed trello 1, added reset css, added server.js, confirmed index and css is connnected

---
+++ server.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+
+const cors = require('cors');
+app.use(cors());
+
+const superagent = require('superagent');
+require('dotenv').config();
+
+const PORT = process.env.PORT || 3000;
+
+//set the view engine to ejs
+app.set('view engine', 'ejs');
+app.use(express.static('./public/../'));
+app.use(express.urlencoded({extended:true}));
+
+//
+app.get('/hello',(request, response)=>{
+  response.render('pages/index');
+});
+
+app.listen(PORT, () => console.log(`listening on port ${PORT}`));
